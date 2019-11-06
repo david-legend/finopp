a97fa0cfaacafe144558070b526dbb03322da8c2
David Cobbina <davidcobbina@Davids-MacBook-Pro.local>
2019-11-06 05:51:06 +0000

    Login and Sign Up Screen design Implemented

--- lib/const/string_const.dart
+++ lib/const/string_const.dart
@@ -15,7 +15,8 @@ class StringConst {
   static const String DEVELOPER = "Volunteer";
   static const String PROFESSION = "Flutter Developer";
   static const String DEV_EMAIL = "niebin312@gmail.com";
-  static const String SIGN_UP = "Sing Up";
+  static const String SIGN_UP = "Sign Up";
+  static const String SIGN_IN = "Sign In";
   static const String SIGN_UP_TEXT = "Don't have an account?";
 
   //SharedPreferences
--- lib/screens/registration/signup_screen.dart
+++ lib/screens/registration/signup_screen.dart
@@ -1,4 +1,12 @@
+import 'package:finop/screens/registration/login_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'package:finop/const/color_const.dart';
+import 'package:finop/const/gradient_const.dart';
+import 'package:finop/const/images_const.dart';
+import 'package:finop/const/size_const.dart';
+import 'package:finop/const/string_const.dart';
+import 'package:finop/widgets/signup_arrow_button.dart';
 
 class SignUpScreen extends StatefulWidget {
 
@@ -9,8 +17,163 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController _username = TextEditingController();
+  TextEditingController _password = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    final _media = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: SIGNUP_BACKGROUND,
+        ),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Stack(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 60.0, horizontal: 40),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Center(
+                          child: Image.asset(
+                            RegistrationImagePath.SignUpLogo,
+                            height: _media.height / 7,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          "WELCOME!",
+                          style: TextStyle(
+                            letterSpacing: 4,
+                            fontFamily: "Montserrat",
+                            fontWeight: FontWeight.bold,
+                            fontSize: TEXT_LARGE_SIZE,
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        Text(
+                          'Sign up',
+                          style: TextStyle(
+                              fontFamily: "Montserrat",
+                              fontWeight: FontWeight.w200,
+                              fontSize: 40),
+                        ),
+                        Text(
+                          'to continue.',
+                          style: TextStyle(
+                              fontFamily: "Montserrat",
+                              fontWeight: FontWeight.w200,
+                              fontSize: 40),
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        Container(
+                          height: _media.height / 3.8,
+                          decoration: BoxDecoration(
+                            gradient: SIGNUP_CARD_BACKGROUND,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black12,
+                                blurRadius: 15,
+                                spreadRadius: 8,
+                              ),
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: Column(
+                              children: <Widget>[
+                                Expanded(
+                                  child: inputText("USERNAME",
+                                      'example@gmail.com', _username, false),
+                                ),
+                                Divider(
+                                  height: 5,
+                                  color: Colors.black,
+                                ),
+                                Expanded(
+                                    child: inputText(
+                                        "PASSWORD", '******', _password, true)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        StringConst.SIGN_UP_TEXT,
+                        style: TextStyle(color: MAIN_COLOR),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      GestureDetector(
+                        onTap: () => Navigator.pushNamed(context, LoginScreen.ROUTE_NAME),
+                        child: Text(StringConst.SIGN_IN),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 50,
+                  )
+                ],
+              ),
+              Positioned(
+                bottom: _media.height / 6.3,
+                right: 15,
+                child: SignUpArrowButton(
+                  icon: IconData(0xe901, fontFamily: 'Icons'),
+                  iconSize: 9,
+                  onTap: () => print("Signup Tapped"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget inputText(
+      String fieldName,
+      String hintText,
+      TextEditingController controller,
+      bool obSecure,
+      ) {
+    return TextField(
+      style: TextStyle(height: 1.3),
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        labelText: fieldName,
+        labelStyle: TextStyle(
+          fontSize: TEXT_NORMAL_SIZE,
+          fontFamily: "Montserrat",
+          fontWeight: FontWeight.w400,
+          letterSpacing: 1,
+          height: 0,
+        ),
+        border: InputBorder.none,
+      ),
+      obscureText: obSecure,
+    );
   }
 }
